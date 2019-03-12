da7ecaaa7b165fa3acae082af7a99ee3ad06e229
Aleksandr <kostinfw@gmail.com>
2019-03-13 01:14:15 +0400

    Spring in Action ch02

---
+++ src/main/java/tacos/domain/Ingredient.java
@@ -0,0 +1,17 @@
+package tacos.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}

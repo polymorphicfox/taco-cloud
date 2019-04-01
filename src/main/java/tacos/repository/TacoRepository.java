744dd73cd3c07b05a7acb3db5382f2c30870829d
Aleksandr Kostin <kostinfw@gmail.com>
2019-04-01 16:29:31 +0400

    Spring in Action ch06 - refactoring

--- src/main/java/tacos/repository/TacoRepository.java
+++
@@ -1,8 +0,0 @@
-package tacos.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import tacos.domain.Taco;
-
-public interface TacoRepository
-        extends CrudRepository<Taco, Long> {
-}
--- src/main/java/tacos/security/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package tacos.security;
-
-import org.springframework.data.repository.CrudRepository;
-import tacos.domain.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-    User findByUsername(String username);
-}
